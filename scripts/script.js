b2a532fc67b4c6a57e2cdebfc72524a3f389b361
Justice Jatau <69175551+JusticeJatau@users.noreply.github.com>
2023-11-13 06:53:26 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 
+const text = document.querySelector(".text")
+        
+const animateText = () =>{
+    setTimeout(() => {
+        text.textContent = "Hi, My name is Justice"
+    }, 0);
+    setTimeout(() => {
+        text.textContent = "Am a webdeveloper"
+    }, 10000);
+    setTimeout(() => {
+        text.textContent = "It's nice meeting you in code!"
+    }, 20000);
+}
+
+animateText()
+setInterval(animateText, 30000);
+
+
